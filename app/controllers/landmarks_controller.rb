4aaf4c23d313c3f133345157eb65c5fba533c923
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-27 00:29:27 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -7,16 +7,8 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     #binding.pry
      @landmark = Landmark.create(params["landmark"])
-     if !params[:landmark][:name].empty?
-       @figure.landmarks << Landmark.create(params[:landmark])
-     end
-
-     if !params[:title][:name].empty?
-       @figure.titles << Title.create(params[:title])
-     end
-
-     @figure.save
-     redirect to "/figures/#{@figure.id}"
+     @landmark.save
+     redirect to "/landmarks/#{@landmark.id}"
    end
 
 
